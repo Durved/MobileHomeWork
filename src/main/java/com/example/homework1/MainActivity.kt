756619653d3c51fb3de83a2267b3e59d08c321f8
Durved <durved96@gmail.com>
2020-12-21 17:26:00 +0300

    First commit

---
+++ src/main/java/com/example/homework1/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.homework1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toActivity1Intent = Intent(this@MainActivity, Activity1::class.java)
+        val toActivity2Intent = Intent(this@MainActivity, Activity2::class.java)
+        val toActivity3Intent = Intent(this@MainActivity, Activity3::class.java)
+        val toActivity4Intent = Intent(this@MainActivity, Activity4::class.java)
+
+        toActivity1Button.setOnClickListener { startActivity(toActivity1Intent) }
+        toActivity2Button.setOnClickListener { startActivity(toActivity2Intent) }
+        toActivity3Button.setOnClickListener { startActivity(toActivity3Intent) }
+        toActivity4Button.setOnClickListener { startActivity(toActivity4Intent) }
+    }
+}
