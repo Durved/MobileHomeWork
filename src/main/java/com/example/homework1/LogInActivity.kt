5e69aa42645e4264744269bce64d6ceab3a2bd0a
Durved <durved96@gmail.com>
2020-12-21 18:41:43 +0300

    Добавлена Форма регистрации, реализована проверка заполниности полей

--- src/main/java/com/example/homework1/LogInActivity.kt
+++ src/main/java/com/example/homework1/LogInActivity.kt
@@ -2,10 +2,41 @@ package com.example.homework1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        RegistrationButton.setOnClickListener {
+
+            if (EmailTextView.text.isBlank() ||
+                    NameTextView.text.isBlank() ||
+                    PasswordTextView.text.isBlank() ||
+                    ConfirmTextView.text.isBlank()) {
+
+                Toast.makeText(this@LogInActivity, "Заполните все поля", Toast.LENGTH_SHORT)
+
+                return@setOnClickListener
+            }
+
+            if (!EmailTextView.text.contains("@"))
+            {
+                EmailTextView.setError("Некорректный E-mail")
+
+                return@setOnClickListener
+            }
+
+            if (PasswordTextView.text != ConfirmTextView)
+            {
+                Toast.makeText(this@LogInActivity, "Пароли не совпадают", Toast.LENGTH_SHORT)
+
+                return@setOnClickListener
+            }
+
+            Toast.makeText(this@LogInActivity, "Всё корректно", Toast.LENGTH_SHORT)
+        }
     }
 }
